43311be9917bb3e88549bc555d5d893847866b3e
andreasalmgren <andreas.almgren16@gmail.com>
2022-01-23 18:29:16 +0100

    la till lite logik

--- src/game/TakePinsGame.java
+++ src/game/TakePinsGame.java
@@ -12,29 +12,52 @@ public class TakePinsGame {
 		computer.Player("Råbot");
 		Random rand = new Random();
 		
-		while (board.noPins > 0) {
+		while (true) {
 			int y;
-			//* int x */
-			System.out.print("How many pins do you want to take?(1 or 2) or face the endless wrath of MökMonstret: \n> ");
+
+			System.out.print("How many pins do you want to take? (1 or 2) or face the endless wrath of MökMonstret: \n> ");
 			Scanner scanner = new Scanner(System.in);
 			String input = scanner.nextLine();
-			int number = 0;
+			int x = 0;
 
+			/** 
+			* Försöker omvandla sträng till int, och om detta inte går återupprepas 
+			* ett felmeddelande tills en sträng som kan omvandlas till en int ges.
+			* Kollar även så att korrekt antal pins (1 eller 2) väljs
+			*/
 			while (true) {
 				try {
-					number = Integer.parseInt(input);
-					break; 
+					x = Integer.parseInt(input);
+					if (1 > x || x > 2) {
+						System.out.println("Chosen amount of pins is incorrect. MökMonstret is coming for you..."); 
+					} else {
+						break; 
+					}
 				} catch (Exception e) {
-					System.out.println("Invalid data type");
+					System.out.print("Invalid data type. Try again. \n");
 				}
-				System.out.print("How many pins do you want to take?(1 or 2) or face the endless wrath of MökMonstret: \n> ");
+				System.out.print("How many pins do you want to take? (1 or 2) or face the endless wrath of MökMonstret: \n> ");
 				input = scanner.nextLine();
 			}
-			
-			human.takePins(board, number);
+
+			/** Avgör om human tar sista pinnen och förlorar (computer vinner)*/
+			if (board.noPins - x <= 0) {
+				System.out.println("Råbot won!");
+				human.takePins(board, x);
+				break;
+			} else {
+				human.takePins(board, x);
+		    }
+
+			/** Avgör om computer tar sista pinnen och förlorar (human vinner)*/
 			y = rand.nextInt(2) + 1;
-			computer.takePins(board, y);
+			if (board.noPins - y <= 0) {
+				System.out.println("Råbert won!");
+				computer.takePins(board, y);
+				break;
+			} else {
+				computer.takePins(board, y);
+		    }
 		}
-		System.out.println("Game Over!");
 	}
 }
